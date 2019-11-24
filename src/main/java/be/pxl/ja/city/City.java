89b707371d6ff02d6381a6eca8162c43e5a13e31
laurens mignolet <laurens.mignolet@student.pxl.be>
2019-11-24 18:34:43 +0100

    initial commit

---
+++ src/main/java/be/pxl/ja/city/City.java
@@ -0,0 +1,31 @@
+package be.pxl.ja.city;
+
+public class City {
+	private String name;
+	private double latitude;
+	private double longitude;
+
+	public City(String name, double latitude, double longitude) {
+		this.name = name;
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+
+	/*
+		Source: https://www.geodatasource.com/developers/java
+	    Calculates the distance between 2 points when given latitude and longitude in decimal degrees
+
+		double radTheta = Math.toRadians(longitude - other.longitude);
+		double radLatitude = Math.toRadians(latitude);
+		double radOtherLatitude = Math.toRadians(other.latitude);
+		double dist = Math.sin(radLatitude) * Math.sin(radOtherLatitude) + Math.cos(radLatitude) * Math.cos(radOtherLatitude) * Math.cos(radTheta);
+		dist = Math.acos(dist);
+		dist = Math.toDegrees(dist);
+		dist = dist * 60 * 1.1515 * 1.609344;
+	 */
+}
