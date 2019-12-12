b57d6e35b02fb4f074afcb298683e85bf2e70c69
laurens mignolet <laurens.mignolet@student.pxl.be>
2019-12-12 22:56:52 +0100

    upi

--- src/main/java/be/pxl/ja/image/ImageArt.java
+++ src/main/java/be/pxl/ja/image/ImageArt.java
@@ -1,10 +1,15 @@
 package be.pxl.ja.image;
 import be.pxl.ja.common.DistanceUtil;
 import javax.swing.*;
+import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 
@@ -20,22 +25,22 @@ public class ImageArt {
 
         Scanner input = new Scanner(System.in);
         System.out.println("please give the name of the picture you want to convert");
-        System.out.println("The image must be located in your resources folder: ");
+        System.out.println("The image must be located in your resources folder (no extension): ");
         String picName = input.next();
 
+        long before = System.currentTimeMillis();
         Loader lod = new Loader("loading... ");
         lod.start();
 
         try {
+            Path path = Paths.get("src\\main\\resources\\" + picName + ".jpg");
+            Path grayPath = Paths.get("src\\main\\resources\\"+ picName + "_grayscale.jpg");
+            Path faireyPath = Paths.get("src\\main\\resources\\"+ picName + "_Fairey.jpg");
 
-            Path p = Paths.get("src\\main\\resources\\" + picName + ".jpg");  
-            Path p2 = Paths.get("src\\main\\resources\\"+ picName + "_grayscale.jpg");
-            Path p3 = Paths.get("src\\main\\resources\\"+ picName + "_Fairey.jpg");
+            List < List < RGBPixel >> RGBList = myReader.readImage(path);
 
-            List < List < RGBPixel >> tokioRGB = myReader.readImage(p);
-
-            List < List < GrayscalePixel >> tokioGray = tokioRGB.stream().map(rgbPixels -> rgbPixels.stream().map(pixel -> pixel.convertToGreyScale()).collect(Collectors.toList())).collect(Collectors.toList());
-            myWriter.writeImage(p2, tokioGray);
+            List < List < GrayscalePixel >> grayList = RGBList.stream().map(rgbPixels -> rgbPixels.stream().map(pixel -> pixel.convertToGreyScale()).collect(Collectors.toList())).collect(Collectors.toList());
+            myWriter.writeImage(grayPath, grayList);
 
             TreeSet < GrayscalePixel > treeGray = new TreeSet < > (new Comparator < GrayscalePixel > () {
                 @Override
@@ -43,16 +48,32 @@ public class ImageArt {
                     return o1.getGreyscale() - o2.getGreyscale();
                 }
             });
-            tokioGray.forEach(grayscalePixels -> grayscalePixels.stream().forEach(grayscalePixel -> treeGray.add(grayscalePixel)));
+
+            /* 1. grayscale pixels are added to the treeset
+             * 2. the map that hold the transformation info is created.
+             * 3. the grayscale pixels are set to the grayscale value of the closes key in from the map.
+             * the closest key is found with the distanceUtil.findclosest() method
+             * 4. the image is written from a stream that takes the value of the grayscale and uses it as a key in the map,
+             * the map gives a RGB pixel, it is used to write the image.
+             * */
+            grayList.forEach(grayscalePixels -> grayscalePixels.stream().forEach(grayscalePixel -> treeGray.add(grayscalePixel)));
             HashMap < GrayscalePixel, RGBPixel > tranlationMap = new HashMap < > (createTranslationMap(faireyColors, treeGray));
-            List < List < GrayscalePixel >> grayfair = tokioGray.stream().map(grayscalePixels -> grayscalePixels.stream().map(grayscalePixel -> DistanceUtil.findClosest(new ArrayList < > (tranlationMap.keySet()), grayscalePixel)).collect(Collectors.toList())).collect(Collectors.toList());
-            myWriter.writeImage(p3, grayfair.stream().map(grayscalePixels -> grayscalePixels.stream().map(grayscalePixel -> tranlationMap.get(grayscalePixel)).collect(Collectors.toList())).collect(Collectors.toList()));
+            List < List < GrayscalePixel >> tranlatedGrayPixelsList = grayList.stream().map(grayscalePixels -> grayscalePixels.stream().map(grayscalePixel -> DistanceUtil.findClosest(new ArrayList < > (tranlationMap.keySet()), grayscalePixel)).collect(Collectors.toList())).collect(Collectors.toList());
+            myWriter.writeImage(faireyPath, tranlatedGrayPixelsList.stream().map(grayscalePixels -> grayscalePixels.stream().map(grayscalePixel -> tranlationMap.get(grayscalePixel)).collect(Collectors.toList())).collect(Collectors.toList()));
 
             lod.stop();
             System.out.println();
-            System.out.println("the grayscale image was succesfully generated at " + p2 );
-            System.out.println("the Fairey image was succesfully generated at " + p3 );
+            System.out.println("the grayscale image was succesfully generated at " + grayPath );
+            System.out.println("the Fairey image was succesfully generated at " + faireyPath );
 
+            long after = System.currentTimeMillis();
+            double seconds  = (after - before) / 1000.0;
+            System.out.println("the program finished in " + seconds + " seconds to process a " + RGBList.get(0).size() + " x " + RGBList.size() + " image" );
+
+            if(Desktop.isDesktopSupported()) {
+                Desktop.getDesktop().open(grayPath.toFile());
+                Desktop.getDesktop().open(faireyPath.toFile());
+            }
         } catch (IOException e) {
             lod.stop();
             System.out.println(e.getMessage());
